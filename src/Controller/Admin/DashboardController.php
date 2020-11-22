992418b79bc1a2420d564f429acdc55d80f52370
Mikkel Ricky <rimi@aarhus.dk>
2020-11-22 22:08:11 +0100

    Security

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,10 +6,12 @@ use App\Entity\Server;
 use App\Entity\Website;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -34,5 +36,13 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToCrud('Websites', 'fa fa-folder-open', Website::class);
         yield MenuItem::linkToCrud('Servers', 'fa fa-folder-open', Server::class);
+    }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        return parent::configureUserMenu($user)
+            ->setMenuItems([
+                // Remove the logout menu item.
+            ]);
     }
 }
